26932e65a424e4a41cf5de2d3781bb78ac2209cd
yusufKaymaz <kaymazyusuf88@gmail.com>
2023-02-19 01:02:28 +0300

    navbar

--- src/App.js
+++ src/App.js
@@ -20,9 +20,12 @@ function App() {
   return (
     <>
       <div className="container mb-5 mt-2">
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
-          <div className="container">
-            <a className="navbar-brand" href="#">
+        <nav
+          className="navbar navbar-expand-lg navbar-dark"
+          style={{ backgroundColor: "#343a40" }}
+        >
+          <div className="container py-2">
+            <a className="navbar-brand text fs-3" href="#">
               Hisse Dünyası
             </a>
             <button
@@ -40,63 +43,40 @@ function App() {
               className="collapse navbar-collapse"
               id="navbarSupportedContent"
             >
-              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0 ms-5">
                 <li className="nav-item">
                   <a className="nav-link active" aria-current="page" href="#">
-                    Home
+                    Borsa
                   </a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="deneme.com">
-                    Link
+                  <a className="nav-link active" aria-current="page" href="#">
+                    Altın
                   </a>
-                </li>
-                <li className="nav-item dropdown">
-                  <a
-                    className="nav-link dropdown-toggle"
-                    href="#"
-                    role="button"
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false"
-                  >
-                    Dropdown
-                  </a>
-                  <ul className="dropdown-menu">
-                    <li>
-                      <a className="dropdown-item" href="#">
-                        Action
-                      </a>
-                    </li>
-                    <li>
-                      <a className="dropdown-item" href="#">
-                        Another action
-                      </a>
-                    </li>
-                    <li>
-                      <hr className="dropdown-divider" />
-                    </li>
-                    <li>
-                      <a className="dropdown-item" href="#">
-                        Something else here
-                      </a>
-                    </li>
-                  </ul>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link disabled">Disabled</a>
+                  <a className="nav-link active" aria-current="page" href="#">
+                    Döviz
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="#">
+                    Kripto
+                  </a>
                 </li>
               </ul>
-              <form className="d-flex" role="search">
-                <input
-                  className="form-control me-2"
-                  type="search"
-                  placeholder="Search"
-                  aria-label="Search"
-                />
-                <button className="btn btn-outline-success" type="submit">
-                  Search
-                </button>
-              </form>
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="#">
+                    Giriş Yap
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="#">
+                    Kayıt Ol
+                  </a>
+                </li>
+              </ul>
             </div>
           </div>
         </nav>
@@ -135,7 +115,13 @@ function App() {
                 {data.map((item) => {
                   return (
                     <tr>
-                      <th><img style={{width:"1.5rem"}} src={item.image} alt={item.name} /></th>
+                      <th>
+                        <img
+                          style={{ width: "1.5rem" }}
+                          src={item.image}
+                          alt={item.name}
+                        />
+                      </th>
                       <th scope="row">{item.name}</th>
                       <td>${item.current_price}</td>
                       <td>10.00</td>

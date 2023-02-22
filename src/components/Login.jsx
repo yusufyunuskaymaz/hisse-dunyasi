65d77df55bbf2ebe58c38f37f425adecbf35e66c
Turgay Levent <turgayleventt@gmail.com>
2023-02-22 15:11:48 +0300

    login4

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,48 @@
 import React from "react";
+import "./login.css";
 
 const Login = () => {
   return (
-    <div className="container mt-5">
-      <div className="row">
-        <div className="col">Login Sayfası</div>
+    <div className="container2">
+      <div className="screen2">
+        <div className="screen__content">
+          <form className="login">
+            <div className="login__field">
+              <i className="login__icon fas fa-user" />
+              <input
+                type="text"
+                className="login__input"
+                placeholder="User name / Email"
+              />
+            </div>
+            <div className="login__field">
+              <i className="login__icon fas fa-lock" />
+              <input
+                type="password"
+                className="login__input"
+                placeholder="Password"
+              />
+            </div>
+            <button className="button login__submit">
+              <span className="button__text">Log In Şİmdi</span>
+              <i className="button__icon fas fa-chevron-right" />
+            </button>
+          </form>
+          <div className="social-login">
+            <h3>log in via</h3>
+            <div className="social-icons">
+              <a href="#" className="social-login__icon fab fa-instagram" />
+              <a href="#" className="social-login__icon fab fa-facebook" />
+              <a href="#" className="social-login__icon fab fa-twitter" />
+            </div>
+          </div>
+        </div>
+        <div className="screen__background">
+          <span className="screen__background__shape screen__background__shape4" />
+          <span className="screen__background__shape screen__background__shape3" />
+          <span className="screen__background__shape screen__background__shape2" />
+          <span className="screen__background__shape screen__background__shape1" />
+        </div>
       </div>
     </div>
   );

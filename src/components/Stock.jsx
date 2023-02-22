17d432468e5a4b5277f4cfee917a0ed742faa50d
yusufKaymaz <kaymazyusuf88@gmail.com>
2023-02-22 14:30:54 +0300

    borsa api

--- src/components/Stock.jsx
+++ src/components/Stock.jsx
@@ -1,85 +1,86 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import io from "socket.io-client";
+const socket = io.connect("http://localhost:3001");
 
 const Stock = () => {
-    const [data, setData] = useState([])
-    const getData = async ()=>{
-        const URL = "https://sore-ruby-chiton-belt.cyclic.app/https://bigpara.hurriyet.com.tr/api/v1/hisse/list"
-        const data = await axios.get(URL, 
-          {headers:{
-            "Access-Control-Allow-Origin": "https://bigpara.hurriyet.com.tr"}})
-        setData(data.data.data)
+  const [data, setData] = useState([]);
+  const [currentMessage, setCurrentMessage] = useState([]);
+  const sendMessage = () => {
+    console.log("first");
+    const messageData = "deneme";
 
-    }
-    useEffect(() => {
-      getData()
-    }, [])
-    console.log(data)
-    // axios.get(url,{headers:{
-    //   Access-Control-Allow-Origin: https://bigpara.hurriyet.com.tr}})
+    socket.emit("send_message", messageData);
+  };
+
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      console.log(data);
+      setData(data);
+    });
+    sendMessage();
+  }, [socket]);
 
   return (
     <div className="container">
-        <div className="row">
-            <div className="col-lg-8">
-                <h1>Stock</h1>
-                <table
-                  className="table table-striped table-hover table-sm"
-                //   style={{ width: "15rem" }}
-                >
-                  <thead>
-                    <tr
-                      className="text-white"
-                      style={{ background: "#464c52" }}
-                    >
-                      <th scope="col" colSpan="12">
-                        BIST100 Tablo
-                      </th>
-                      
-                    </tr>
-                  </thead>
-                  <thead>
-                    <tr>
-                      <th scope="col">Menkul Adı </th>
-                      <th scope="col">Son</th>
-                      <th scope="col">Alış</th>
-                      <th scope="col">Yüksek</th>
-                      <th scope="col">Düşük</th>
-                      <th scope="col">A. ort</th>
-                      <th scope="col">%</th>
-                      <th scope="col">A. ort</th>
-                      <th scope="col">A. ort</th>
-                      <th scope="col">Hacim(Lot)</th>
-                      <th scope="col">Hacim(Tl)</th>
-                      <th scope="col">Son İşlem</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {data.map((item)=>{
-                      return (
-                        <tr>
-                      <th scope="row">{item.kod}</th>
-                      <td>3.984</td>
-                      <td>3.984</td>
-                      <td>10.00</td>
-                      <td>18.10</td>
-                      <td>3.984</td>
-                      <td>3.984</td>
-                      <td>10.00</td>
-                      <td>18.10</td>
-                      <td>3.984</td>
-                      <td>10.00</td>
-                      <td>18.10</td>
-                    </tr>
-                      )
-                    })}
-                    
-                  </tbody>
-                </table>
-            </div>
-        </div>
-    </div>
-  )
-}
+      <button className="btn btn-warning" onClick={sendMessage}>Yenile</button>
 
-export default Stock
+      <div className="row">
+        <div className="col-lg-8">
+          <h1>Stock</h1>
+          <table
+            className="table table-striped  table-sm"
+            //   style={{ width: "15rem" }}
+          >
+            <thead>
+              <tr className="text-white" style={{ background: "#464c52" }}>
+                <th scope="col" colSpan="13">
+                  BIST100 Tablo
+                </th>
+              </tr>
+            </thead>
+            <thead>
+              <tr>
+                <th scope="col">Menkul Adı </th>
+                <th scope="col">Son</th>
+                <th scope="col">Alış</th>
+                <th scope="col">Satış</th>
+                <th scope="col">Yüksek</th>
+                <th scope="col">Düşük</th>
+                <th scope="col">A. ort</th>
+                <th scope="col">%</th>
+                <th scope="col">Hacim(Lot)</th>
+                <th scope="col">Hacim(Tl)</th>
+                <th scope="col">Son İş.</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((item, index) => {
+                return (
+                  <tr key={index}>
+                    <th scope="row">{item.title}</th>
+                    <td>{item.son}</td>
+                    <td>{item.alis}</td>
+                    <td>{item.satis}</td>
+                    <td>{item.yuksek}</td>
+                    <td>{item.dusus}</td>
+                    <td>{item.aylik_ort}</td>
+                    <td>{item.yuzde}</td>
+                    <td>{item.hacim_lot}</td>
+                    <td>{item.hacim_tl}</td>
+                    <td>{item.son_islem}</td>
+                    {/* <td>{item.son}</td>
+                      <td>{item.son}</td> */}
+                    <td></td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Stock;

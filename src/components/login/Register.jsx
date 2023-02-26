413ffba7b6dec6c2a1867922556b508febefa15e
Turgay Levent <turgayleventt@gmail.com>
2023-02-26 17:56:45 +0300

    register6

--- src/components/login/Register.jsx
+++ src/components/login/Register.jsx
@@ -6,15 +6,21 @@ const Register = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(userName);
+  };
+
   return (
     <div className="container2">
       <div className="screen2">
         <div className="screen__content">
-          <form className="login">
+          <form className="login" onSubmit={handleSubmit}>
             <div className="login__field">
               <i className="login__icon fas fa-user" />
               <input
                 type="text"
+                required
                 className="login__input"
                 placeholder="User Name"
                 onChange={(e) => setUserName(e.target.value)}
@@ -24,6 +30,7 @@ const Register = () => {
               <i className="login__icon fas fa-envelope" />
               <input
                 type="text"
+                required
                 className="login__input"
                 placeholder="Email"
                 onChange={(e) => setEmail(e.target.value)}
@@ -34,12 +41,13 @@ const Register = () => {
               <i className="login__icon fas fa-lock" />
               <input
                 type="password"
+                required
                 className="login__input"
                 placeholder="Password"
                 onChange={(e) => setPassword(e.target.value)}
               />
             </div>
-            <button className="button login__submit">
+            <button className="button login__submit" type="submit">
               <span className="button__text">Register</span>
               <i className="button__icon fas fa-chevron-right" />
             </button>

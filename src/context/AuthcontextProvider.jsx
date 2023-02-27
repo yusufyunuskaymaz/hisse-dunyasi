8fb5d2755e2cb1081b92c89895be0957a82b76df
Turgay Levent <turgayleventt@gmail.com>
2023-02-27 12:35:34 +0300

    5

--- src/context/AuthcontextProvider.jsx
+++ src/context/AuthcontextProvider.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { createContext } from "react";
+import { userObserver } from "./auth/firebase";
 
 export const AuthContext = createContext();
 
-const AuthcontextProvider = ({ children }) => {
-  return <AuthContext.Provider value={null}>{children}</AuthContext.Provider>;
+const AuthContextProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(false);
+  useEffect(() => {
+    userObserver();
+  }, []);
+  return (
+    <AuthContext.Provider value={{ currentUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
-export default AuthcontextProvider;
+export default AuthContextProvider;

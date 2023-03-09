ba667271f01ab219c9f8f68815a22004a027948c
yusufKaymaz <kaymazyusuf88@gmail.com>
2023-03-09 21:55:05 +0300

    stock-detail

--- src/components/comment/YasalUyari.jsx
+++ src/components/comment/YasalUyari.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const YasalUyari = () => {
   return (
-    <div >
+    <div className='mb-5'>
           <span style={{ color: "red" }}>Yasal Uyarı:</span>{" "}
        <p
        style={{fontSize:".9rem"}}

d5d74c33005b557dd7269dbf1c865987eb23e70d
Turgay Levent <turgayleventt@gmail.com>
2023-03-09 16:10:32 +0300

    stiky

--- src/components/stock/StickySidebar.jsx
+++ src/components/stock/StickySidebar.jsx
@@ -1,18 +1,17 @@
-import React from 'react'
-import Kazananlar from './TvWidgets/Kazananlar'
-
+import React from "react";
+import Kazananlar from "./TvWidgets/Kazananlar";
 
 const StickySidebar = () => {
   return (
-    <div className="row stickSidebar d-none d-md-block">
-        <div className="col">
-          <div className="">
-            <Kazananlar />
-            <div className="stockDetail mb-5"></div>
-          </div>
+    <div className="row stickSidebar d-none d-lg-block">
+      <div className="col">
+        <div className="">
+          <Kazananlar />
+          <div className="stockDetail mb-5"></div>
         </div>
       </div>
-  )
-}
+    </div>
+  );
+};
 
-export default StickySidebar
+export default StickySidebar;

ec8e742705d414ea250e2faa1016fa05648483f7
Turgay Levent <turgayleventt@gmail.com>
2023-03-15 12:38:21 +0300

    footer1

--- src/components/NavbarFooter/Footer.jsx
+++ src/components/NavbarFooter/Footer.jsx
@@ -1,20 +1,49 @@
 import React from "react";
+import { NavLink } from "react-bootstrap";
+import Card from "react-bootstrap/Card";
+import CardGroup from "react-bootstrap/CardGroup";
 
-const Footer = () => {
+function Footer() {
   return (
-    <div className="container-fluid">
-      <div className="row">
-        <div className="col p-0">
-          <footer className="bg-dark text-white text-center fs-6">
-            <p className="py-3 mb-0">
-              Tüm hakları saklıdır. Made by Happy Coders{" "}
-              {new Date().getFullYear()}
-            </p>
-          </footer>
-        </div>
-      </div>
-    </div>
+    <CardGroup>
+      <Card bg="secondary" text="white">
+        <Card.Body>
+          <Card.Title>
+            <strong> HİSSE DÜNYASI </strong>
+          </Card.Title>
+          <Card.Text>
+            Güncel Borsa, Altın, Döviz ve Kripto Para Endeksleri.
+          </Card.Text>
+          <div className="footermenu">
+            <NavLink href="stock" className="navbar-white">
+              Borsa
+            </NavLink>
+            <NavLink href="/currency">Döviz</NavLink>
+          </div>
+          <div className="footermenu">
+            <NavLink href="/gold">Altın</NavLink>
+            <NavLink href="/crypto">Kripto</NavLink>
+          </div>
+        </Card.Body>
+      </Card>
+      <Card bg="secondary" text="white">
+        <Card.Body>
+          <Card.Title>
+            <strong> YASAL UYARI </strong>{" "}
+          </Card.Title>
+          <Card.Text>
+            <strong>hissedunyasi.com</strong> da yer alan bilgi, yorum ve
+            tavsiyeler Yatırım Danışmanlığı kapsamında değildir. Yorumlar
+            kullanıcıların kişisel görüşlerinden ibarettir. Bu görüş ve
+            bilgilere dayanılarak alınacak yatırım kararları beklentilerinize
+            uygun sonuçlar doğurmayabilir. Dolayısıyla kullanıcıların
+            yorumlarına göre yatırım kararı almamanız konusunda kesinlikle
+            uyarıyoruz.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </CardGroup>
   );
-};
+}
 
 export default Footer;

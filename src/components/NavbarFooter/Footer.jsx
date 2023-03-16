520c1065555d16fd9e42faf82cb3017b9e595b99
yusufKaymaz <kaymazyusuf88@gmail.com>
2023-03-16 15:28:20 +0300

    footer

--- src/components/NavbarFooter/Footer.jsx
+++ src/components/NavbarFooter/Footer.jsx
@@ -6,14 +6,12 @@ import Nav from "react-bootstrap/Nav";
 
 function Footer() {
   return (
-    <div className="mt-4 " style={{ fontSize: ".5rem" }}>
-      <CardGroup m-2>
-        <Card>
-          <Card.Body>
-            <Card.Title>
-              <strong>BORSA</strong>
-            </Card.Title>
-            <div class="col-xl-4 offset-xl-2 col-lg-4 col-md-6">
+    <div className="mt-4" style={{ fontSize: ".5rem" }}>
+        
+           <div className="container-fluid p-3 ps-5 pt-5 footer " style={{backgroundColor:"#231b1b"}}>
+            <div className="row justify-content-center">
+            <div class=" col-lg-2">
+              <h5>BORSA</h5>
               <div>
                 <ul className="list-unstyled">
                   <li>
@@ -56,123 +54,186 @@ function Footer() {
                 </ul>
               </div>
             </div>
-          </Card.Body>
-        </Card>
-        <Card>
-          <Card.Body>
-            <Card.Title>
-              <strong>DÖVİZ</strong>
-            </Card.Title>
-            <div class="col-xl-4 offset-xl-2 col-lg-4 col-md-6">
+            <div class=" col-lg-2">
+              <h5>BORSA</h5>
               <div>
                 <ul className="list-unstyled">
                   <li>
-                    <Link to="currency" class="text-decoration-none">
-                      Dolar
+                    <Nav.Link
+                      href="stock/akbank"
+                      className="text-decoration-none"
+                    >
+                      Akbank
+                    </Nav.Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="stock/kardemir-(d)"
+                      className="text-decoration-none"
+                    >
+                      Kardemir
                     </Link>
                   </li>
                   <li>
-                    <Link to="stock" class="text-decoration-none">
-                      Euro
+                    <Link to="stock/sise-cam" className="text-decoration-none">
+                      Şişecam
                     </Link>
                   </li>
                   <li>
-                    <Link to="stock" class="text-decoration-none">
-                      Sterlin
+                    <Link
+                      to="stock/turk-hava%20yollari"
+                      class="text-decoration-none"
+                    >
+                      THY
                     </Link>
                   </li>
                   <li>
-                    <Link to="stock" class="text-decoration-none">
-                      Yen
-                    </Link>
-                  </li>
-                  <li>
-                    <Link to="stock" class="text-decoration-none">
-                      Ruble
+                    <Link
+                      to="stock/yapi-ve%20kredi%20bank."
+                      class="text-decoration-none"
+                    >
+                      Yapı Kredi
                     </Link>
                   </li>
                 </ul>
               </div>
             </div>
-          </Card.Body>
-        </Card>
-        <Card>
-          <Card.Body>
-            <Card.Title>
-              <strong>ALTIN</strong>
-            </Card.Title>
-            <div class="col-xl-4 offset-xl-2 col-lg-4 col-md-6">
+            <div class=" col-lg-2 ">
+              <h5>BORSA</h5>
               <div>
                 <ul className="list-unstyled">
                   <li>
-                    <Link to="stock" class="text-decoration-none">
-                      Gram Altın
+                    <Nav.Link
+                      href="stock/akbank"
+                      className="text-decoration-none"
+                    >
+                      Akbank
+                    </Nav.Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="stock/kardemir-(d)"
+                      className="text-decoration-none"
+                    >
+                      Kardemir
                     </Link>
                   </li>
                   <li>
-                    <Link to="stock" class="text-decoration-none">
-                      Çeyrek Altın
+                    <Link to="stock/sise-cam" className="text-decoration-none">
+                      Şişecam
                     </Link>
                   </li>
                   <li>
-                    <Link to="stock" class="text-decoration-none">
-                      Yarım Altın
+                    <Link
+                      to="stock/turk-hava%20yollari"
+                      class="text-decoration-none"
+                    >
+                      THY
                     </Link>
                   </li>
                   <li>
-                    <Link to="stock" class="text-decoration-none">
-                      Tam Altın
-                    </Link>
-                  </li>
-                  <li>
-                    <Link to="stock" class="text-decoration-none">
-                      Ata Altın
+                    <Link
+                      to="stock/yapi-ve%20kredi%20bank."
+                      class="text-decoration-none"
+                    >
+                      Yapı Kredi
                     </Link>
                   </li>
                 </ul>
               </div>
             </div>
-          </Card.Body>
-        </Card>
-        <Card>
-          <Card.Body>
-            <Card.Title>
-              <strong>KRİPTO</strong>
-            </Card.Title>
-            <div class="col-xl-4 offset-xl-2 col-lg-4 col-md-6">
+            <div class=" col-lg-2 ">
+              <h5>BORSA</h5>
               <div>
                 <ul className="list-unstyled">
                   <li>
-                    <Link to="stock" class="text-decoration-none">
-                      Bitcoin
+                    <Nav.Link
+                      href="stock/akbank"
+                      className="text-decoration-none"
+                    >
+                      Akbank
+                    </Nav.Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="stock/kardemir-(d)"
+                      className="text-decoration-none"
+                    >
+                      Kardemir
                     </Link>
                   </li>
                   <li>
-                    <Link to="stock" class="text-decoration-none">
-                      Ethereum
+                    <Link to="stock/sise-cam" className="text-decoration-none">
+                      Şişecam
                     </Link>
                   </li>
                   <li>
-                    <Link to="stock" class="text-decoration-none">
-                      Tether
+                    <Link
+                      to="stock/turk-hava%20yollari"
+                      class="text-decoration-none"
+                    >
+                      THY
                     </Link>
                   </li>
                   <li>
-                    <Link to="stock" class="text-decoration-none">
-                      BNB
-                    </Link>
-                  </li>
-                  <li>
-                    <Link to="stock" class="text-decoration-none">
-                      USD Coin
+                    <Link
+                      to="stock/yapi-ve%20kredi%20bank."
+                      class="text-decoration-none"
+                    >
+                      Yapı Kredi
                     </Link>
                   </li>
                 </ul>
               </div>
             </div>
-          </Card.Body>
-        </Card>
-      </CardGroup>
+            <div class=" col-lg-4 ">
+              <h5>BORSA</h5>
+              <div>
+                <ul className="list-unstyled">
+                  <li>
+                    <Nav.Link
+                      href="stock/akbank"
+                      className="text-decoration-none"
+                    >
+                      Akbank
+                    </Nav.Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="stock/kardemir-(d)"
+                      className="text-decoration-none"
+                    >
+                      Kardemir
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="stock/sise-cam" className="text-decoration-none">
+                      Şişecam
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="stock/turk-hava%20yollari"
+                      class="text-decoration-none"
+                    >
+                      THY
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="stock/yapi-ve%20kredi%20bank."
+                      class="text-decoration-none"
+                    >
+                      Yapı Kredi
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            </div>
+           </div>
+          
+         
       <Card.Footer
         className="text-muted text-center"
         style={{ fontSize: "1rem" }}

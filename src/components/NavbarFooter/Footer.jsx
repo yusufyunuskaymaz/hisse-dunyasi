5e54ce9f2732bdbacea486c6c86ba90600a187c8
Turgay Levent <turgayleventt@gmail.com>
2023-03-16 13:33:54 +0300

    footer1

--- src/components/NavbarFooter/Footer.jsx
+++ src/components/NavbarFooter/Footer.jsx
@@ -1,104 +1,164 @@
 import React from "react";
-import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
+import CardGroup from "react-bootstrap/CardGroup";
+import { Link } from "react-router-dom";
 
 function Footer() {
   return (
-    <Card className="mt-5">
-      <div className="container">
-        <Card.Header className="mt-3"></Card.Header>
-        <div className="row">
-          <div className="col-xl-2 col-lg-2 col-md-6">
+    <CardGroup mt-2>
+      <Card>
+        <Card.Body>
+          <Card.Title>
+            <strong>BORSA</strong>
+          </Card.Title>
+          <div class="col-xl-4 offset-xl-2 col-lg-4 col-md-6">
             <div>
-              <h3>
-                <strong> Hisse Dünyası </strong>
-              </h3>
-              <p class="mb-30 footer-desc">
-                Güncel Borsa, Altın, Döviz ve Kripto Para Endeksleri
-              </p>
-            </div>
-          </div>
-          <div class="col-xl-2 offset-xl-1 col-lg-2 col-md-6">
-            <div>
-              <h4>Keşfet</h4>
               <ul className="list-unstyled">
                 <li>
-                  <a href="#" class="text-decoration-none">
-                    Anasayfa
-                  </a>
-                </li>
-                <li>
-                  <a href="stock" class="text-decoration-none">
+                  <Link to="stock" class="text-decoration-none">
                     Borsa
-                  </a>
+                  </Link>
                 </li>
                 <li>
-                  <a href="currency" class="text-decoration-none">
-                    Döviz
-                  </a>
+                  <Link to="stock" class="text-decoration-none">
+                    Borsa
+                  </Link>
                 </li>
                 <li>
-                  <a href="gold" class="text-decoration-none">
-                    Altın
-                  </a>
+                  <Link to="stock" class="text-decoration-none">
+                    Borsa
+                  </Link>
                 </li>
                 <li>
-                  <a href="crypto" class="text-decoration-none">
-                    Kripto
-                  </a>
+                  <Link to="stock" class="text-decoration-none">
+                    Borsa
+                  </Link>
+                </li>
+                <li>
+                  <Link to="stock" class="text-decoration-none">
+                    Borsa
+                  </Link>
                 </li>
               </ul>
             </div>
           </div>
-          <div className="col-xl-2 col-lg-2 col-md-6">
+        </Card.Body>
+      </Card>
+      <Card>
+        <Card.Body>
+          <Card.Title>
+            <strong>BORSA</strong>
+          </Card.Title>
+          <div class="col-xl-4 offset-xl-2 col-lg-4 col-md-6">
             <div>
-              <h4>Service</h4>
               <ul className="list-unstyled">
                 <li>
-                  <a href="#" class="text-decoration-none">
-                    Marketing
-                  </a>
+                  <Link to="stock" class="text-decoration-none">
+                    Borsa
+                  </Link>
                 </li>
                 <li>
-                  <a href="#" class="text-decoration-none">
-                    Branding
-                  </a>
+                  <Link to="stock" class="text-decoration-none">
+                    Borsa
+                  </Link>
                 </li>
                 <li>
-                  <a href="#" class="text-decoration-none">
-                    Web Design
-                  </a>
+                  <Link to="stock" class="text-decoration-none">
+                    Borsa
+                  </Link>
                 </li>
                 <li>
-                  <a href="#" class="text-decoration-none">
-                    Graphics Design
-                  </a>
+                  <Link to="stock" class="text-decoration-none">
+                    Borsa
+                  </Link>
+                </li>
+                <li>
+                  <Link to="stock" class="text-decoration-none">
+                    Borsa
+                  </Link>
                 </li>
               </ul>
             </div>
           </div>
-          <div className="col-xl-5 col-lg-6 col-md-6">
+        </Card.Body>
+      </Card>
+      <Card>
+        <Card.Body>
+          <Card.Title>
+            <strong>BORSA</strong>
+          </Card.Title>
+          <div class="col-xl-4 offset-xl-2 col-lg-4 col-md-6">
             <div>
-              <h4>YASAL UYARI</h4>
-              <div>
-                <p>
-                  hissedunyasi.com'da yer alan bilgi, yorum ve tavsiyeler
-                  Yatırım Danışmanlığı kapsamında değildir. Yorumlar
-                  kullanıcıların kişisel görüşlerinden ibarettir. Bu görüş ve
-                  bilgilere dayanılarak alınacak yatırım kararları
-                  beklentilerinize uygun sonuçlar doğurmayabilir. Dolayısıyla
-                  kullanıcıların yorumlarına göre yatırım kararı almamanız
-                  konusunda kesinlikle uyarıyoruz.
-                </p>
-              </div>
+              <ul className="list-unstyled">
+                <li>
+                  <Link to="stock" class="text-decoration-none">
+                    Borsa
+                  </Link>
+                </li>
+                <li>
+                  <Link to="stock" class="text-decoration-none">
+                    Borsa
+                  </Link>
+                </li>
+                <li>
+                  <Link to="stock" class="text-decoration-none">
+                    Borsa
+                  </Link>
+                </li>
+                <li>
+                  <Link to="stock" class="text-decoration-none">
+                    Borsa
+                  </Link>
+                </li>
+                <li>
+                  <Link to="stock" class="text-decoration-none">
+                    Borsa
+                  </Link>
+                </li>
+              </ul>
             </div>
           </div>
-        </div>
-        <Card.Footer className="text-muted text-center">
-          © 2023 HİSSE DÜNYASI - Her hakkı saklıdır.
-        </Card.Footer>
-      </div>
-    </Card>
+        </Card.Body>
+      </Card>
+      <Card>
+        <Card.Body>
+          <Card.Title>
+            <strong>BORSA</strong>
+          </Card.Title>
+          <div class="col-xl-4 offset-xl-2 col-lg-4 col-md-6">
+            <div>
+              <ul className="list-unstyled">
+                <li>
+                  <Link to="stock" class="text-decoration-none">
+                    Borsa
+                  </Link>
+                </li>
+                <li>
+                  <Link to="stock" class="text-decoration-none">
+                    Borsa
+                  </Link>
+                </li>
+                <li>
+                  <Link to="stock" class="text-decoration-none">
+                    Borsa
+                  </Link>
+                </li>
+                <li>
+                  <Link to="stock" class="text-decoration-none">
+                    Borsa
+                  </Link>
+                </li>
+                <li>
+                  <Link to="stock" class="text-decoration-none">
+                    Borsa
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </Card.Body>
+      </Card>
+    </CardGroup>
   );
 }
 

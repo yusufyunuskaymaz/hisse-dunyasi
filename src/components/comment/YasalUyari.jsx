fe5fdcfd6144b1a69c63673080b030e00d6f1bd1
Turgay Levent <turgayleventt@gmail.com>
2023-03-17 14:59:57 +0300

    yasal1

--- src/components/comment/YasalUyari.jsx
+++ src/components/comment/YasalUyari.jsx
@@ -5,7 +5,7 @@ const YasalUyari = () => {
     <div className="mb-5">
       <span style={{ color: "red" }}>Yasal Uyarı:</span>{" "}
       <p style={{ fontSize: ".9rem" }}>
-        hissedunyasi.com'de yer alan bilgi, yorum ve tavsiyeler Yatırım
+        hissedunyasi.com'da yer alan bilgi, yorum ve tavsiyeler Yatırım
         Danışmanlığı kapsamında değildir. Yorumlar kullanıcıların kişisel
         görüşlerinden ibarettir. Bu görüş ve bilgilere dayanılarak alınacak
         yatırım kararları beklentilerinize uygun sonuçlar doğurmayabilir.

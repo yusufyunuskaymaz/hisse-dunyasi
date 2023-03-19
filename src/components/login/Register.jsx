d71b28cde6eb44686dd5b56a1e5daba3143422a5
Turgay Levent <turgayleventt@gmail.com>
2023-03-19 17:53:57 +0300

    loginregister

--- src/components/login/Register.jsx
+++ src/components/login/Register.jsx
@@ -1,87 +1,193 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router";
 import { Link } from "react-router-dom";
-import { createUser, signUpWithGoogle } from "../../auth/firebase";
-import "./login.css";
+import { signIn, signUpWithGoogle, createUser } from "../../auth/firebase";
+import {
+  MDBContainer,
+  MDBTabs,
+  MDBTabsItem,
+  MDBTabsLink,
+  MDBTabsContent,
+  MDBTabsPane,
+  MDBBtn,
+  MDBIcon,
+  MDBInput,
+  MDBCheckbox,
+} from "mdb-react-ui-kit";
 
 const Register = () => {
+  const [justifyActive, setJustifyActive] = useState("tab1");
   const [userName, setUserName] = useState("");
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
 
-  const handleSubmit = (e) => {
+  const handleSubmitRegister = (e) => {
     e.preventDefault();
     const displayName = `${userName}`;
     createUser(email, password, navigate, displayName);
+  };
 
-    // console.log(userName);
+  const handleSubmitLogin = (e) => {
+    e.preventDefault();
+    signIn(email, password, navigate);
+
+    // console.log(email, password);
   };
 
   const handleGoogleProvider = () => {
     signUpWithGoogle(navigate);
   };
 
+  const handleJustifyClick = (value) => {
+    if (value === justifyActive) {
+      return;
+    }
+
+    setJustifyActive(value);
+  };
+
   return (
-    <div className="container2">
-      <div className="screen2">
-        <div className="screen__content">
-          <form className="login" onSubmit={handleSubmit}>
-            <div className="login__field">
-              <i className="login__icon fas fa-user" />
-              <input
-                type="text"
-                required
-                className="login__input"
-                placeholder="User Name"
-                onChange={(e) => setUserName(e.target.value)}
-              />
-            </div>
-            <div className="login__field">
-              <i className="login__icon fas fa-envelope" />
-              <input
-                type="text"
-                required
-                className="login__input"
-                placeholder="Email"
-                onChange={(e) => setEmail(e.target.value)}
-              />
+    <MDBContainer className="p-3 my-5 d-flex flex-column w-50">
+      <MDBTabs
+        pills
+        justify
+        className="mb-3 d-flex flex-row justify-content-between"
+      >
+        <MDBTabsItem>
+          <MDBTabsLink
+            onClick={() => handleJustifyClick("tab1")}
+            active={justifyActive === "tab1"}
+          >
+            Login
+          </MDBTabsLink>
+        </MDBTabsItem>
+        <MDBTabsItem>
+          <MDBTabsLink
+            onClick={() => handleJustifyClick("tab2")}
+            active={justifyActive === "tab2"}
+          >
+            Register
+          </MDBTabsLink>
+        </MDBTabsItem>
+      </MDBTabs>
+
+      <MDBTabsContent>
+        <MDBTabsPane show={justifyActive === "tab1"}>
+          <div className="text-center mb-3">
+            <p>Sign in with:</p>
+
+            <div
+              className="d-flex justify-content-between mx-auto"
+              style={{ width: "40%" }}
+            >
+              <MDBBtn
+                tag="a"
+                color="none"
+                className="m-1"
+                style={{ color: "#1266f1" }}
+                onClick={handleGoogleProvider}
+              >
+                <MDBIcon fab icon="google" size="sm" />
+              </MDBBtn>
             </div>
 
-            <div className="login__field">
-              <i className="login__icon fas fa-lock" />
-              <input
-                type="password"
-                required
-                className="login__input"
-                placeholder="Password"
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </div>
-            <button className="button login__submit" type="submit">
-              <span className="button__text">Register</span>
-              <i className="button__icon fas fa-chevron-right" />
-            </button>
-          </form>
-          <div className="social-login">
-            <h3>Register with</h3>
-            <div className="social-icons">
-              <button
-                onClick={handleGoogleProvider}
-                type="button"
-                className="social-login__icon fab fa-google"
-              />
-            </div>
+            <p className="text-center mt-3">or:</p>
           </div>
-        </div>
-        <div className="screen__background">
-          <span className="screen__background__shape screen__background__shape4" />
-          <span className="screen__background__shape screen__background__shape3" />
-          <span className="screen__background__shape screen__background__shape2" />
-          <span className="screen__background__shape screen__background__shape1" />
-        </div>
-      </div>
-    </div>
+
+          <MDBInput
+            wrapperClass="mb-4"
+            label="Email address"
+            id="form1"
+            type="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <MDBInput
+            wrapperClass="mb-4"
+            label="Password"
+            id="form2"
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <div className="d-flex justify-content-between mx-4 mb-4">
+            <MDBCheckbox
+              name="flexCheck"
+              value=""
+              id="flexCheckDefault"
+              label="Remember me"
+            />
+            <a href="!#">Forgot password?</a>
+          </div>
+
+          <MDBBtn className="mb-4 w-100" onSubmit={handleSubmitLogin}>
+            Sign in
+          </MDBBtn>
+          <p className="text-center">
+            Not a member? <a href="#!">Register</a>
+          </p>
+        </MDBTabsPane>
+
+        <MDBTabsPane show={justifyActive === "tab2"}>
+          <div className="text-center mb-3">
+            <p>Sign un with:</p>
+
+            <div
+              className="d-flex justify-content-between mx-auto"
+              style={{ width: "40%" }}
+            >
+              <MDBBtn
+                tag="a"
+                color="none"
+                className="m-1"
+                style={{ color: "#1266f1" }}
+                onClick={handleGoogleProvider}
+              >
+                <MDBIcon fab icon="google" size="sm" />
+              </MDBBtn>
+            </div>
+
+            <p className="text-center mt-3">or:</p>
+          </div>
+
+          <MDBInput
+            wrapperClass="mb-4"
+            label="Username"
+            id="form1"
+            type="text"
+            onChange={(e) => setUserName(e.target.value)}
+          />
+          <MDBInput
+            wrapperClass="mb-4"
+            label="Email"
+            id="form1"
+            type="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <MDBInput
+            wrapperClass="mb-4"
+            label="Password"
+            id="form1"
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <div className="d-flex justify-content-center mb-4">
+            <MDBCheckbox
+              name="flexCheck"
+              id="flexCheckDefault"
+              label="I have read and agree to the terms"
+            />
+          </div>
+
+          <MDBBtn className="mb-4 w-100" onSubmit={handleSubmitRegister}>
+            Sign up
+          </MDBBtn>
+        </MDBTabsPane>
+      </MDBTabsContent>
+    </MDBContainer>
   );
 };
+
 export default Register;
